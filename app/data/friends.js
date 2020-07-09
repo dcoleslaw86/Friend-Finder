6e1ef601d845d61448f9da280b39558c906a8741
dcoleslaw86 <cole.desrosiers86@gmail.com>
2020-07-08 22:58:04 -0500

    adding fully functioning app and all its componets

---
+++ app/data/friends.js
@@ -0,0 +1,117 @@
+var friends = [
+    {
+      name: "Scott Summers",
+      photo: "https://i.pinimg.com/originals/b6/56/c8/b656c873b9ab412e7dec42417c3fde47.jpg",
+      scores: [
+        "5",
+        "1",
+        "4",
+        "4",
+        "5",
+        "1",
+        "2",
+        "5",
+        "4",
+        "1"
+      ]
+    },
+    {
+      name: "Logan",
+      photo: "https://i.pinimg.com/564x/59/00/57/5900575e8b300ee7767f8c832c1bf27b.jpg",
+      scores: [
+        "4",
+        "2",
+        "5",
+        "1",
+        "3",
+        "2",
+        "2",
+        "1",
+        "3",
+        "2"
+      ]
+    },
+    {
+      name: "Rogue",
+      photo: "https://i.pinimg.com/originals/a1/39/01/a13901a93c72320a0889392c0e134ef8.jpg",
+      scores: [
+        "5",
+        "2",
+        "2",
+        "2",
+        "4",
+        "1",
+        "3",
+        "2",
+        "5",
+        "5"
+      ]
+    },
+    {
+      name: "Ororo Munroe",
+      photo: "https://i.pinimg.com/originals/af/2c/c1/af2cc102f57e77e67ac5550ff9997c38.jpg",
+      scores: [
+        "3",
+        "3",
+        "4",
+        "2",
+        "2",
+        "1",
+        "3",
+        "2",
+        "2",
+        "3"
+      ]
+    },
+    {
+      name: "Dr. Henry 'Hank' McCoy",
+      photo: "https://i.pinimg.com/originals/f9/ff/5f/f9ff5faf4be80703f55ef2ad4dacc083.jpg",
+      scores: [
+        "4",
+        "3",
+        "4",
+        "1",
+        "5",
+        "2",
+        "5",
+        "3",
+        "1",
+        "4"
+      ]
+    },
+    {
+      name: "Charles Xavier",
+      photo: "https://i.pinimg.com/originals/87/f5/3d/87f53dfa1c0161afbedfc6e5b6e8d7db.jpg",
+      scores: [
+        "1",
+        "5",
+        "5",
+        "5",
+        "5",
+        "1",
+        "5",
+        "1",
+        "5",
+        "5"
+      ]
+    },
+    {
+        name: "Wade Willson",
+        photo: "https://i.pinimg.com/564x/51/a1/d7/51a1d704b00f5489f65329a83b23b6b1.jpg",
+        scores: [
+          "1",
+          "1",
+          "1",
+          "1",
+          "1",
+          "1",
+          "1",
+          "1",
+          "1",
+          "1"
+        ]
+      }
+  ];
+  
+  module.exports = friends;
+  
